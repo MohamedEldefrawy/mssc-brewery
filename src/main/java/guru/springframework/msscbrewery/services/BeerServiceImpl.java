035fec39e18efa70fa4352e708b8eecdf46a1714
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-09-12 16:33:19 +0200

    Add create beer endpoint

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,20 +1,27 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
-import org.springframework.stereotype.Service;
-
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 2019-04-20.
  */
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
-    @Override
-    public BeerDto getBeerById(UUID beerId) {
-        return BeerDto.builder().id(UUID.randomUUID())
-                .beerName("Galaxy Cat")
-                .beerStyle("Pale Ale")
-                .build();
-    }
+  @Override
+  public BeerDto getBeerById(UUID beerId) {
+    return BeerDto.builder().id(UUID.randomUUID())
+        .beerName("Galaxy Cat")
+        .beerStyle("Pale Ale")
+        .build();
+  }
+
+  @Override
+  public BeerDto create(BeerDto beerDto) {
+    log.debug("Creating....");
+    return BeerDto.builder().id(UUID.randomUUID()).beerName(beerDto.getBeerName()).build();
+  }
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,10 +2,13 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,11 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    ResponseEntity<BeerDto> createBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(this.beerService.create(beerDto), HttpStatus.CREATED);
     }
 
 }
