3e56da16020d3a1964e4ec06ae8027d680b803b9
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-09-17 11:48:32 +0200

    add validation to name attribute

--- src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -1,18 +1,24 @@
 package guru.springframework.msscbrewery.web.model;
 
+import java.util.UUID;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDto {
-    private UUID id;
-    private String name;
+
+  private UUID id;
+  @NotNull
+  @Max(value = 100)
+  @Min(value = 3)
+  private String name;
 
 }
